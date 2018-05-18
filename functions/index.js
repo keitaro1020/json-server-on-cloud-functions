f26b1bdc0c070f8f2d92edcaf20e9c936ad75c1d
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-05-18 12:55:24 +0900

    add json-server

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+exports.mock = functions.https.onRequest(server);
+
